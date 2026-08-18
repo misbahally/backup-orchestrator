7b612306542be54a01cdd00f5cc9329a34b56866
Misbah Ally <2109168+misbahally@users.noreply.github.com>
2026-08-18 09:29:25 +0400

    Testing workflow trigger: Bump version to 0.3.7

--- libs/orchestrator_core/orchestrator_core/__init__.py
+++ libs/orchestrator_core/orchestrator_core/__init__.py
@@ -2,7 +2,7 @@ from .database import build_engine, build_session_local
 from .models import BackupRun, Binding, Destination, RunStatus, Source, SourceType
 from .secret_resolver import resolve_secret_mapping, resolve_secret_text
 
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 
 __all__ = [
     "__version__",
